5c03cbf92e143796898fd63060d9879a2cd9cbfb
mateja <mateja176@gmail.com>
2020-08-21 16:53:45 +0200

    feat(client/pages/Dashboard): adds tables

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,11 @@
 import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import moment from 'moment';
@@ -17,6 +24,18 @@ import { NavItems } from '../utils/navItems';
 
 const Dashboard: React.FC<RouteComponentProps> = () => {
   const { prediction, input } = React.useContext(AppContext);
+
+  const rows = [
+    {
+      name: 'Frozen yoghurt',
+      calories: 159,
+      fat: 6.0,
+      carbs: 24,
+      protein: 4.0,
+    },
+  ];
+
+  console.log(prediction);
 
   return (
     <Box p={4}>
@@ -50,15 +69,110 @@ const Dashboard: React.FC<RouteComponentProps> = () => {
         </Box>
       </Box>
       <Typography variant={'h2'}>Prediction</Typography>
-      <Box>
-        {prediction ? (
-          <pre>{JSON.stringify(prediction, null, 2)}</pre>
-        ) : (
-          <Typography>
-            <Link to={NavItems.upload}>Upload CSV data</Link> to see predictions
-          </Typography>
-        )}
-      </Box>
+      {prediction ? (
+        <Box mt={3}>
+          <Box mb={2}>
+            <Box mb={1}>
+              <Typography variant={'h3'}>Importance per feature</Typography>
+            </Box>
+            <Box>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      {Object.keys(prediction.featureImportance).map((key) => (
+                        <TableCell key={key}>{key}</TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow key={row.name}>
+                        {Object.values(prediction.featureImportance).map(
+                          (value) => (
+                            <TableCell key={value}>{value}</TableCell>
+                          ),
+                        )}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
+          </Box>
+          <Box mb={2}>
+            <Box mb={1}>
+              <Typography variant={'h3'}>Scores</Typography>
+            </Box>
+            <Box my={2}>
+              <Box mb={1}>
+                <Typography variant={'h4'}>Overall scores</Typography>
+              </Box>
+              <Box>
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        {Object.keys(prediction.metrics.overallScores).map(
+                          (key) => (
+                            <TableCell key={key}>{key}</TableCell>
+                          ),
+                        )}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow key={row.name}>
+                          {Object.values(prediction.metrics.overallScores).map(
+                            (value) => (
+                              <TableCell key={value}>{value}</TableCell>
+                            ),
+                          )}
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Box>
+            </Box>
+            <Box my={2}>
+              <Box mb={1}>
+                <Typography variant={'h4'}>Confusion Metric</Typography>
+              </Box>
+              <Box>
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        {Object.keys(prediction.metrics.confusionMetric).map(
+                          (key) => (
+                            <TableCell key={key}>{key}</TableCell>
+                          ),
+                        )}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow key={row.name}>
+                          {Object.values(
+                            prediction.metrics.confusionMetric,
+                          ).map((value) => (
+                            <TableCell key={value}>{value}</TableCell>
+                          ))}
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      ) : (
+        <Typography>
+          <Link to={NavItems.upload}>Upload CSV data</Link> to see predictions
+        </Typography>
+      )}
     </Box>
   );
 };
